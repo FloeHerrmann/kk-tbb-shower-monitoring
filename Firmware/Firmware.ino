83a27773bc64812de43ba472bcd63077286b15e0
Florian Herrmann <florian.herrmann@m2m-systems.de>
2014-05-29 11:11:38 +0200

    Refactoring
    Some refactoring of the source code

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,129 +1,178 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Author:    m2m systems GmbH, Florian Herrmann                             */
+/* Copyright: 2014, m2m systems GmbH, Florian Herrmann                       */
+/* Purpose:                                                                  */
+/*   Monitoring and measuring the water consuption and temperatures of a     */
+/*   shower to calculate the costs for taking this shower.                   */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include <EEPROM.h>
 #include <SPI.h>
 #include <GD2.h>
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+// Define own data types
+#define ubyte uint8_t
+#define uint uint16_t
+#define ulong uint32_t
+
+// Number of samples that will be displayed in the charts
 #define SAMPLES 20
-#define SAMPLESOFFSET 11
-#define SAMPLEINTERVALL 3000
 
-#define FLOWSENSORPIN 5
+// Milliseconds between two samples
+#define SAMPLES_INTERVALL 3000
 
-float PulseValues[20];
-long SampleStartTime;
-int numberOfSamples;
+// Interrupt number 5 = Arduino pin 18
+#define FLOW_SENSOR_PIN 5
 
-boolean ResetButtonPressed;
-boolean SettingsButtonPressed;
+// Divider to get the water amout from the flow sensor impulses
+#define FLOW_SENSOR_DIVIDER 100
 
-volatile uint16_t pulses = 0;
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+// Array for storing the flow sensor samples
+float WaterFlowValues[SAMPLES];
+
+// Array for storing the temperature samples
+float TemperatureValues[SAMPLES];
+
+// Is true if the "Reset" button is pressed, otherwise false
+boolean ResetButtonIsPressed = false;
+
+// Is true if the "Settings" icon is pressed, otherwise false
+boolean SettingsButtonIsPressed = false;
+
+// Helper variable for taking samples
+ulong SampleTimeHelper;
+
+// Helper variable for the number of taken samples
+uint NumberOfSamplesHelper = 0;
+
+// Helper variable for the x-axis difference between two values
+uint XAxisFactor = 0;
+
+// Counter for the pulses of the flow sensor
+volatile ulong FlowSensorPulses = 0;
+volatile ulong TotalFlowSensorPulses = 0;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void setup() {
 	Serial.begin( 57600 );
 
-	for( uint16_t index = 0 ; index < SAMPLES ; index++ ) PulseValues[ index ] = -1.0;
+	// Initialize value arrays
+	for( uint16_t index = 0 ; index < SAMPLES ; index++ ) {
+		WaterFlowValues[ index ] = -1.0;
+		TemperatureValues[ index ] = -1.0;
+	}
 
+	// Initialize display
 	GD.begin();
 	LoadBackground();
 	DrawBackground();
 	DrawTouchTags();
 	GD.swap();
 
-	pinMode( 16 , OUTPUT );
-	digitalWrite( 16 , LOW );
-	attachInterrupt( FLOWSENSORPIN , COUNT , FALLING) ;
+	// Initialize variables
+	SampleTimeHelper = millis();
+	XAxisFactor = (int)( 222.0 / (float)SAMPLES );
 
-	SampleStartTime = millis();
-	numberOfSamples = 0;
-	ResetButtonPressed = false;
-	SettingsButtonPressed = false;
+	// Attach an interrupt for the flow sensor
+	attachInterrupt( FLOW_SENSOR_PIN , CountImpulses , FALLING) ;
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void loop () {
+
+	// Get the input of the touch display and check if something relevant is pressed
 	GD.get_inputs();
 	if( GD.inputs.tag == 1 ) {
-		ResetButtonPressed = true;
+		ResetButtonIsPressed = true;
 	} else if( GD.inputs.tag == 2 ) {
-		SettingsButtonPressed = true;
+		SettingsButtonIsPressed = true;
 	} else {
-		if( ResetButtonPressed == true ) {
+		if( ResetButtonIsPressed == true ) {
 			Serial.println( "Reset" );
 			delay( 50 );
-			ResetButtonPressed = false;
-		} else if( SettingsButtonPressed == true ) {
+			ResetButtonIsPressed = false;
+		} else if( SettingsButtonIsPressed == true ) {
 			Serial.println( "Settings" );
 			delay( 50 );
-			SettingsButtonPressed = false;
+			SettingsButtonIsPressed = false;
 		}
 	}
-	
-	if( millis() - SampleStartTime >= SAMPLEINTERVALL ) {
-		if( numberOfSamples < SAMPLES ) {
-			float pulsesF = ((float)pulses)/100.0;
-			PulseValues[ numberOfSamples++ ] = pulsesF;
-			pulses = 0;
+
+
+	if( millis() - SampleTimeHelper >= SAMPLES_INTERVALL ) {
+		float pulsesF = ((float)FlowSensorPulses) / ((float)FLOW_SENSOR_DIVIDER);
+		if( NumberOfSamplesHelper < SAMPLES ) {
+			WaterFlowValues[ NumberOfSamplesHelper++ ] = pulsesF;
 		} else {
-			for( uint16_t index = 0 ; index < (SAMPLES-1) ; index++ ) PulseValues[ index ] = PulseValues[ index + 1];
-			float pulsesF = ((float)pulses)/100.0;
-			PulseValues[ 19 ] = pulsesF;
-			pulses = 0;
+			for( uint index = 0 ; index < (SAMPLES-1) ; index++ ) {
+				WaterFlowValues[ index ] = WaterFlowValues[ index + 1 ];
+			}
+			WaterFlowValues[ (SAMPLES-1) ] = pulsesF;
 		}
+		TotalFlowSensorPulses += FlowSensorPulses;
+		FlowSensorPulses = 0;
 		DrawBackground();
 		DrawTouchTags();
 		DrawWaterConsumption();
 		GD.swap();
-		SampleStartTime = millis();
+		SampleTimeHelper = millis();
 	}
 }
 
-void COUNT( ){
-	pulses = pulses + 1;
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void CountImpulses( ){
+	FlowSensorPulses = FlowSensorPulses + 1;
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void DrawWaterConsumption(){
-	float OffsetFactor = 96.0 / 3.0;
 
-	GD.ColorRGB(151,187,205);
-	GD.LineWidth(1 * 10);
-  	GD.Begin(LINE_STRIP);
+	float OffsetFactor = 96.0 / 4.0;
+
+	GD.ColorRGB( 151 , 187 , 205 );
+	GD.LineWidth( 1 * 10 );
+  	GD.Begin( LINE_STRIP );
+
   	for( uint16_t index = 0 ; index < SAMPLES ; index++ ) {
-  		if( PulseValues[index] != -1.0 ) {
-			float xOffsetF = PulseValues[index] * OffsetFactor;
+  		if( WaterFlowValues[index] != -1.0 ) {
+			float xOffsetF = WaterFlowValues[ index ] * OffsetFactor;
 			int xOffset = (int)xOffsetF;
-			int yOffset = index * SAMPLESOFFSET;
+			int yOffset = index * XAxisFactor;
 			GD.Vertex2ii( 335 + xOffset , 11 + yOffset );
 		}
 	}
 
-	GD.PointSize(16 * 2);
-	GD.ColorRGB(151,187,205);
-	GD.Begin(POINTS);
+	GD.ColorRGB( 151 , 187 , 205 );
+	GD.PointSize( 16 * 2 );
+	GD.Begin( POINTS );
+
 	for( uint16_t index = 0 ; index < SAMPLES ; index++ ) {
-		if( PulseValues[index] != -1.0 ) {
-			float xOffsetF = PulseValues[index] * OffsetFactor;
+		if( WaterFlowValues[ index ] != -1.0 ) {
+			float xOffsetF = WaterFlowValues[ index ] * OffsetFactor;
 			int xOffset = (int)xOffsetF;
-			int yOffset = index * SAMPLESOFFSET;
+			int yOffset = index * XAxisFactor;
 			GD.Vertex2ii( 335 + xOffset , 11 + yOffset );
 		}
 	}
 
-	//GD.SaveContext();
 	for( uint16_t index = 0 ; index < (SAMPLES-1) ; index++ ) {
-		if( PulseValues[index+1] != -1.0 ) {
-			GD.ColorRGB( 151,187,205 );
+		if( WaterFlowValues[ index + 1 ] != -1.0 ) {
+			GD.ColorRGB( 151 , 187 , 205 );
 			GD.ColorA( 80 );
 			Poly waterFlow;
 			waterFlow.begin();
-			// Point One
-			int xOffset = (int)(PulseValues[index] * OffsetFactor);
-			int xCordinate = (16 * 335) + 16 * xOffset;
-			int yCordinate = (16 * 11) + 16 * SAMPLESOFFSET * index;
+			int xOffset = (int)( WaterFlowValues[ index ] * OffsetFactor );
+			int xCordinate = ( 16 * 335 ) + 16 * xOffset;
+			int yCordinate = ( 16 * 11 ) + 16 * XAxisFactor * index;
 			waterFlow.v( 16 * 335 , yCordinate );
 			waterFlow.v( xCordinate , yCordinate );
-			// Point Two
-			xOffset = (int)(PulseValues[index+1] * OffsetFactor);
-			xCordinate = (16 * 335) + 16 * xOffset;
-			yCordinate = (16 * 11) + 16 * SAMPLESOFFSET * (index+1);
+			xOffset = (int)( WaterFlowValues[ index + 1 ] * OffsetFactor );
+			xCordinate = ( 16 * 335 ) + 16 * xOffset;
+			yCordinate = ( 16 * 11 ) + 16 * XAxisFactor * ( index + 1 );
 			waterFlow.v( xCordinate , yCordinate );
 			waterFlow.v( 16 * 335 , yCordinate );
 			waterFlow.draw();
@@ -131,6 +180,7 @@ void DrawWaterConsumption(){
 	}
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void DrawBackground(){
 	GD.ClearColorRGB( 0xFFFFFF );
 	GD.Clear();
@@ -138,11 +188,13 @@ void DrawBackground(){
 	GD.Vertex2ii( 0 , 0 );
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void LoadBackground(){
 	GD.cmd_loadimage( 0 , 0 );
 	GD.load( "bg2.jpg" );
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void DrawTouchTags(){
 	GD.ColorRGB( 0x00FF00 );
 	GD.ColorA( 100 );
